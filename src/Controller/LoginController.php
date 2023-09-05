80682c6f373d1fd1262bbea9a2572fba45131867
Aleksandar Zivanovic <acas_mail@yahoo.com>
2023-09-05 17:58:38 +0200

    Added registration with email verification

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -9,9 +9,15 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class LoginController extends AbstractController
 {
+    
     #[Route('/login', name: 'app_login')]
     public function index(AuthenticationUtils $authenticationUtils): Response
     {
+        // block access to the page for logged in users
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_home');
+        }
+
         $error = $authenticationUtils->getLastAuthenticationError();
         $lastUsername = $authenticationUtils->getLastUsername();
 
