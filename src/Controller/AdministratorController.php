ceda1223e52e5ecc36d7e27fd69f7d09afa5d9b3
Aleksandar Zivanovic <acas_mail@yahoo.com>
2023-09-07 22:55:50 +0200

    Added list of all users to the admin page & created and implemented deleteUser() method

--- src/Controller/AdministratorController.php
+++ src/Controller/AdministratorController.php
@@ -2,18 +2,35 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class AdministratorController extends AbstractController
 {
     #[Route('/administrator', name: 'app_administrator')]
-    public function index(): Response
+    public function index(UserRepository $userRepository): Response
     {
+        $users = $userRepository->findAll();
+
         return $this->render('administrator/index.html.twig', [
-            'controller_name' => 'AdministratorController',
+            'users' => $users,
         ]);
+    }
+
+    #[Route('/delete-user/{id}', methods: ['GET', 'DELETE'], name: 'app_delete_user')]
+    public function deleteUser($id, EntityManagerInterface $em, UserRepository $userRepository): RedirectResponse
+    {
+        $user = $userRepository->find($id);
+        $em->remove($user);
+        $em->flush();
+
+        $this->addFlash('deletedUser', 'Delete user with ID '.  $id);
+
+        return $this->redirectToRoute('app_administrator');
     }
 }
