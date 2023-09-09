ca33e937832da5276a9c5a01cbe5e6b94d95d568
Aleksandar Zivanovic <acas_mail@yahoo.com>
2023-09-09 14:41:17 +0200

    PROBLEM: twig forme prijavljuje gresku: Object of class App\Entity\User could not be converted to string

--- src/Controller/AdministratorController.php
+++ src/Controller/AdministratorController.php
@@ -3,12 +3,16 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Entity\UserProfile;
+use App\Form\EditUserType;
+use App\Form\ProfileType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 class AdministratorController extends AbstractController
 {
@@ -32,5 +36,49 @@ class AdministratorController extends AbstractController
         $this->addFlash('deletedUser', 'Delete user with ID '.  $id);
 
         return $this->redirectToRoute('app_administrator');
+    }
+
+    #[Route('/edit-user/{user}', name: 'app_edit_user')]
+    public function editUser(User $user, EntityManagerInterface $entityManager, Request $request): Response
+    {
+        $form = $this->createForm(EditUserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getData();
+            $entityManager->persist($user);
+            $entityManager->flush();
+            $this->addFlash('userEdited', 'User data is edited!');
+            return $this->redirectToRoute('app_administrator');
+        }
+
+        return $this->render('administrator/edit_user.html.twig', [
+            'form' => $form,
+            'user' => $user,
+        ]);
+    }
+
+    #[Route('/edit-profile/{id}', name: 'app_edit_user')]
+    public function editProfile($id, EntityManagerInterface $entityManager, Request $request, UserRepository $userRepository): Response
+    {
+        // $user = $entityManager->getRepository(User::class)->find($id);
+        $user = $userRepository->find($id);
+        $userProfile = $user->getUserProfile();
+        
+        // dd($userProfile);
+        $form = $this->createForm(ProfileType::class, $userProfile);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $userProfile = $form->getData();
+            $entityManager->persist($userProfile);
+            $entityManager->flush();
+            $this->addFlash('userEdited', 'Profile is edited!');
+            return $this->redirectToRoute('app_administrator');
+        }
+
+        return $this->render('administrator/edit_user.html.twig', [
+            'form' => $form,
+        ]);
     }
 }
---
+++ src/Form/ProfileType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\UserProfile;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProfileType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('userName')
+            ->add('address')
+            ->add('phone')
+            ->add('country')
+            ->add('user_id')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => UserProfile::class,
+        ]);
+    }
+}
